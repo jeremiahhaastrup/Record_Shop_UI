a9d6b496dbca86098ea9490eb6955f9acb00e2c8
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-18 15:03:15 +0100

    feat: declare and initialise activityMainBinding and mainActivtyViewModel variables

--- app/src/main/java/com/northcoders/recordshopapplication/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/mainactivity/MainActivity.java
@@ -7,20 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.northcoders.recordshopapplication.R;
+import com.northcoders.recordshopapplication.databinding.ActivityMainBinding;
+import com.northcoders.recordshopapplication.model.Album;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Album> albums;
+    private AlbumAdapter albumAdapter;
+    private MainActivityViewModel mainActivityViewModel;
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
     }
 }
