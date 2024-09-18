a313d15de4cb044d28a3d6478940fdce2192edfa
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-18 15:06:04 +0100

    feat: implement getAllAlbums() in main activity class

--- app/src/main/java/com/northcoders/recordshopapplication/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/mainactivity/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +17,7 @@ import com.northcoders.recordshopapplication.databinding.ActivityMainBinding;
 import com.northcoders.recordshopapplication.model.Album;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,17 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        getAllAlbums();
+    }
+
+    private void getAllAlbums() {
+        mainActivityViewModel.getAllAlbums().observe(this, new Observer<List<Album>>() {
+            @Override
+            public void onChanged(List<Album> albumsFromLiveData) {
+                albums = (ArrayList<Album>) albumsFromLiveData;
+                displayInRecyclerView();
+            }
+        });
     }
 }
