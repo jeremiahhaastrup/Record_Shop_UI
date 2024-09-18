4eb207420b10b541dd27a1712b3d4c0efeafb367
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-18 12:06:13 +0100

    feat: implementation of MainActivityViewModel layer

--- app/src/main/java/com/northcoders/recordshopapplication/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/mainactivity/MainActivityViewModel.java
@@ -1,2 +1,26 @@
-package com.northcoders.recordshopapplication.ui.mainactivity;public class MainActivityViewModel {
+package com.northcoders.recordshopapplication.ui.mainactivity;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.northcoders.recordshopapplication.model.Album;
+import com.northcoders.recordshopapplication.repository.AlbumRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    AlbumRepository albumRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.albumRepository = new AlbumRepository(application);
+    }
+
+    public LiveData<List<Album>> getAllAlbums() {
+        return albumRepository.getMutableLiveData();
+    }
 }
