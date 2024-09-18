e570f55119ef57f303352d3307ef985b7ac87ce7
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-18 11:46:17 +0100

    feat: retrofit instance to generate an implementation of the AlbumApiService interface

--- app/src/main/java/com/northcoders/recordshopapplication/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/recordshopapplication/service/RetrofitInstance.java
@@ -1,2 +1,28 @@
-package com.northcoders.recordshopapplication.service;public class RetrofitInstance {
+package com.northcoders.recordshopapplication.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://record-shop-env.eba-gcpj2isn.eu-west-2.elasticbeanstalk.com/api/v1/";
+
+    public static AlbumApiService getService() {
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(logging).build();
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit.create(AlbumApiService.class);
+    }
 }
