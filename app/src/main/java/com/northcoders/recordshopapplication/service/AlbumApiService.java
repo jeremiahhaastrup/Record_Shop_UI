bfeb9e694e58ad69e624dff7013beac4921e56c6
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-20 12:08:59 +0100

    feat: implement POST Method to AlbumApiService

--- app/src/main/java/com/northcoders/recordshopapplication/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshopapplication/service/AlbumApiService.java
@@ -5,10 +5,15 @@ import com.northcoders.recordshopapplication.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
 
-    @GET("/albums")
+    @GET("/api/v1/albums")
     Call<List<Album>> getAllAlbums();
+
+    @POST("/api/v1/albums")
+    Call<Album> addAlbum(@Body Album album);
 }
