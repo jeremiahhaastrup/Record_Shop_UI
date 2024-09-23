8d53dac22a4ec3b118c52308feb0425541df89ad
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-23 16:04:56 +0100

    feat: implementation of overrriden onResponse method for ArtistRepository

--- app/src/main/java/com/northcoders/recordshopapplication/repository/ArtistRepository.java
+++ app/src/main/java/com/northcoders/recordshopapplication/repository/ArtistRepository.java
@@ -1,2 +1,44 @@
-package com.northcoders.recordshopapplication.repository;public class ArtistRepository {
+package com.northcoders.recordshopapplication.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.northcoders.recordshopapplication.model.Artist;
+import com.northcoders.recordshopapplication.service.AlbumApiService;
+import com.northcoders.recordshopapplication.service.RetrofitInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ArtistRepository {
+
+    private MutableLiveData<List<Artist>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public ArtistRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<Artist>> getMutableLiveData() {
+        AlbumApiService albumApiService = RetrofitInstance.getService();
+        Call<List<Artist>> call = albumApiService.getAllArtists();
+
+        call.enqueue(new Callback<List<Artist>>(){
+            @Override
+            public void onResponse(Call<List<Artist>> call, Response<List<Artist>> response) {
+                List<Artist> artists = response.body();
+                mutableLiveData.setValue(artists);
+            }
+
+            @Override
+            public void onFailure(Call<List<Artist>> call, Throwable throwable) {
+
+            }
+        });
+        return mutableLiveData;
+    }
 }
