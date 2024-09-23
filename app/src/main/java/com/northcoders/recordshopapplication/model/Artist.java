64c2c24207350fbd57d786ccc3424b553a9ec2bc
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-09-23 15:56:58 +0100

    feat: GSON SerializeName() for Artist Object

--- app/src/main/java/com/northcoders/recordshopapplication/model/Artist.java
+++ app/src/main/java/com/northcoders/recordshopapplication/model/Artist.java
@@ -1,10 +1,23 @@
 package com.northcoders.recordshopapplication.model;
 
-public class Artist {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
 
+import com.google.gson.annotations.SerializedName;
+import com.northcoders.recordshopapplication.BR;
+
+public class Artist extends BaseObservable {
+
+    @SerializedName("artist_id")
     private int artist_id;
+
+    @SerializedName("name")
     private String name;
+
+    @SerializedName("dateOfBirth")
     private String dateOfBirth;
+
+    @SerializedName("placeOfBirth")
     private String placeOfBirth;
 
     public Artist(String name, int artist_id, String dateOfBirth, String placeOfBirth) {
@@ -17,35 +30,43 @@ public class Artist {
     public Artist() {
     }
 
+    @Bindable
     public int getArtist_id() {
         return artist_id;
     }
 
     public void setArtist_id(int artist_id) {
         this.artist_id = artist_id;
+        notifyPropertyChanged(BR.artist_id);
     }
 
+    @Bindable
     public String getDateOfBirth() {
         return dateOfBirth;
     }
 
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+        notifyPropertyChanged(BR.dateOfBirth);
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getPlaceOfBirth() {
         return placeOfBirth;
     }
 
     public void setPlaceOfBirth(String placeOfBirth) {
         this.placeOfBirth = placeOfBirth;
+        notifyPropertyChanged(BR.placeOfBirth);
     }
 }
---
+++ app/src/main/java/com/northcoders/recordshopapplication/repository/ArtistRepository.java
@@ -0,0 +1,2 @@
+package com.northcoders.recordshopapplication.repository;public class ArtistRepository {
+}
