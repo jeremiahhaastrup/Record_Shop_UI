b78603b0503fa9ce4150805ec294e5b9b74ce350
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-18 12:22:10 +0100

    feat: int to string and string to int converter

---
+++ app/src/main/java/com/northcoders/recordshopapplication/util/Converter.java
@@ -0,0 +1,33 @@
+package com.northcoders.recordshopapplication.util;
+
+import androidx.databinding.InverseMethod;
+
+public class Converter {
+    @InverseMethod("stringToInt")
+    public static String intToString(int value) {
+        if (value == 0) {
+            return "";
+        }
+        return String.valueOf(value);
+    }
+
+    public static int stringToInt(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    @InverseMethod("stringToIntDisplay")
+    public static String intToStringDisplay(int value) {
+        return String.valueOf(value);
+    }
+
+    public static int stringToIntDisplay(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
