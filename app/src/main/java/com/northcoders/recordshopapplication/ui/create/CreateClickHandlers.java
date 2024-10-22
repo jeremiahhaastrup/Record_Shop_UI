fbe6198c204859cf7f0b6e8c295dfa8048a9e5e0
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-22 11:41:00 +0100

    feat: alertDialog for cancel button

--- app/src/main/java/com/northcoders/recordshopapplication/ui/create/CreateClickHandlers.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/create/CreateClickHandlers.java
@@ -1,6 +1,8 @@
 package com.northcoders.recordshopapplication.ui.create;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
@@ -58,9 +60,18 @@ public class CreateClickHandlers {
 
     }
 
-    public void onBackBtnClicker(View view) {
-        Intent intent = new Intent(view.getContext(), MainActivity.class);
-        context.startActivity(intent);
-
+    public void onCancelBtnClicker(View view) {
+        new AlertDialog.Builder(view.getContext())
+                .setTitle("Back to Home")
+                .setMessage("Are you sure you want to leave this page? All progress will be lost.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(view.getContext(), MainActivity.class);
+                        context.startActivity(intent);
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
