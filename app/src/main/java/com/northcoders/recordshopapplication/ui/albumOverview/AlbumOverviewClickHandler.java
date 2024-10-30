8943ffa115f9437d1fa4cfc29b8decd54f50c7d4
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-30 11:25:59 +0000

    feat: onBackBtnClicked implemented

---
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/albumOverview/AlbumOverviewClickHandler.java
@@ -0,0 +1,32 @@
+package com.northcoders.recordshopapplication.ui.albumOverview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import com.northcoders.recordshopapplication.model.Album;
+import com.northcoders.recordshopapplication.ui.mainactivity.MainActivity;
+import com.northcoders.recordshopapplication.ui.mainactivity.MainActivityAlbumViewModel;
+
+public class AlbumOverviewClickHandler {
+
+    private Context context;
+    private Album album;
+    private MainActivityAlbumViewModel mainActivityAlbumViewModel;
+
+    public AlbumOverviewClickHandler(Context context, Album album, MainActivityAlbumViewModel mainActivityAlbumViewModel) {
+        this.context = context;
+        this.album = album;
+        this.mainActivityAlbumViewModel = mainActivityAlbumViewModel;
+    }
+
+    public void onBackBtnClicked(View view) {
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        context.startActivity(intent);
+    }
+
+    // edit button
+
+
+    // get specific artist
+}
