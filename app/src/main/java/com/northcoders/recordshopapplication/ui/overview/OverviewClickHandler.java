fd0b444bea526b712f117545d2d0c5f5f12ab909
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-30 11:52:59 +0000

    feat: onCancelAlbumEditBtn implemented

--- app/src/main/java/com/northcoders/recordshopapplication/ui/overview/OverviewClickHandler.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/overview/OverviewClickHandler.java
@@ -1,6 +1,8 @@
 package com.northcoders.recordshopapplication.ui.overview;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 
@@ -30,6 +32,22 @@ public class OverviewClickHandler {
         Intent intent = new Intent(view.getContext(), AlbumOverviewActivity.class);
         context.startActivity(intent);
     }
+
+    public void onCancelAlbumEditBrn(View view) {
+        new AlertDialog.Builder(view.getContext())
+                .setTitle("Back to Album")
+                .setMessage("Are you sure you want to leave this page? All progress will be lost.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(view.getContext(), AlbumOverviewActivity.class);
+                        context.startActivity(intent);
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
 
     public void onEditBtnClicked(View view) {
         Intent intent = new Intent(view.getContext(), EditAlbumActivity.class);
