ef122b3126fc613dec53a3fd143c27b5148636ef
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-11-01 18:12:41 +0000

    feat: deleteAlbum method in AlbumRepository

--- app/src/main/java/com/northcoders/recordshopapplication/repository/AlbumRepository.java
+++ app/src/main/java/com/northcoders/recordshopapplication/repository/AlbumRepository.java
@@ -87,4 +87,23 @@ public class AlbumRepository {
             }
         });
     }
+
+    public void deleteAlbum(long id) {
+        AlbumApiService albumApiService = RetrofitInstance.getService();
+        Call<String> call = albumApiService.deleteAlbum(id);
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Toast.makeText(application.getApplicationContext(),
+                        "Album deleted successfully",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable throwable) {
+                Log.e("DELETE REQ", throwable.getMessage());
+            }
+        });
+    }
 }
