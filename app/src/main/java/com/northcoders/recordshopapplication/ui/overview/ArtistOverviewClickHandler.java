a90000d5f38caadbd066aeb86aaa14ca1f1fe425
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-11-04 12:40:48 +0000

    feat: onBackToAlbumFromArtistBtnClicked implemented

--- app/src/main/java/com/northcoders/recordshopapplication/ui/overview/ArtistOverviewClickHandler.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/overview/ArtistOverviewClickHandler.java
@@ -1,4 +1,30 @@
 package com.northcoders.recordshopapplication.ui.overview;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.View;
+
+import com.northcoders.recordshopapplication.model.Artist;
+import com.northcoders.recordshopapplication.ui.mainactivity.MainActivityAlbumViewModel;
+import com.northcoders.recordshopapplication.ui.mainactivity.MainActivityArtistViewModel;
+
 public class ArtistOverviewClickHandler {
+
+    private final Context context;
+    private final Artist artist;
+    private final MainActivityArtistViewModel mainActivityAlbumViewModel;
+
+    public ArtistOverviewClickHandler(Context context, Artist artist, MainActivityArtistViewModel mainActivityAlbumViewModel) {
+        this.context = context;
+        this.artist = artist;
+        this.mainActivityAlbumViewModel = mainActivityAlbumViewModel;
+    }
+
+    public void onBackToAlbumFromArtistBtnClicked(View view) {
+        Intent intent = new Intent(view.getContext(), AlbumOverviewActivity.class);
+        context.startActivity(intent);
+    }
+
 }
