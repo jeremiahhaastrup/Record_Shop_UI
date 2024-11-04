916015b310d7a883776817a15117d42cce657e4c
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-11-04 12:39:53 +0000

    feat: editArtistButton implemented

--- app/src/main/java/com/northcoders/recordshopapplication/ui/overview/ArtistOverviewActivity.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/overview/ArtistOverviewActivity.java
@@ -1,8 +1,11 @@
 package com.northcoders.recordshopapplication.ui.overview;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -12,7 +15,9 @@ import com.bumptech.glide.Glide;
 import com.northcoders.recordshopapplication.R;
 import com.northcoders.recordshopapplication.databinding.ActivityArtistOverviewBinding;
 import com.northcoders.recordshopapplication.model.Artist;
+import com.northcoders.recordshopapplication.ui.edit.EditArtistActivity;
 import com.northcoders.recordshopapplication.ui.mainactivity.MainActivityAlbumViewModel;
+import com.northcoders.recordshopapplication.ui.mainactivity.MainActivityArtistViewModel;
 
 public class ArtistOverviewActivity extends AppCompatActivity {
 
@@ -20,6 +25,8 @@ public class ArtistOverviewActivity extends AppCompatActivity {
     private ActivityArtistOverviewBinding activityArtistOverviewBinding;
     private Artist artist;
     private ArtistOverviewClickHandler handler;
+
+    private ImageButton editArtistButton;
 
     @SuppressLint("NewApi")
     @Override
@@ -29,8 +36,6 @@ public class ArtistOverviewActivity extends AppCompatActivity {
 
         activityArtistOverviewBinding = DataBindingUtil.setContentView(this, R.layout.activity_artist_overview);
         artist = getIntent().getParcelableExtra(ARTIST_KEY);
-        MainActivityAlbumViewModel viewmodel = new ViewModelProvider(this).get(MainActivityAlbumViewModel.class);
-        activityArtistOverviewBinding.setArtist(artist);
 
         if (artist == null) {
             Log.e("ArtistOverviewActivity", "Artist data is null:");
@@ -38,6 +43,8 @@ public class ArtistOverviewActivity extends AppCompatActivity {
             Log.d("ArtistOverviewActivity", "Artist Name: " + artist.getName());
         }
 
+        MainActivityArtistViewModel viewmodel = new ViewModelProvider(this).get(MainActivityArtistViewModel.class);
+        activityArtistOverviewBinding.setArtist(artist);
 
         handler = new ArtistOverviewClickHandler(this, artist, viewmodel);
         activityArtistOverviewBinding.setClickHandler(handler);
@@ -48,5 +55,21 @@ public class ArtistOverviewActivity extends AppCompatActivity {
                 .placeholder(R.drawable.placeholder)
                 .error(R.drawable.error)
                 .into(activityArtistOverviewBinding.addArtistImageView);
+
+        editArtistButton = findViewById(R.id.editArtistButton);
+        editArtistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (artist != null) {
+                    Log.d("ArtistOverviewActivity", "Passing Artist: " + artist.getName());
+                    Intent intent = new Intent(ArtistOverviewActivity.this, EditArtistActivity.class);
+                    intent.putExtra(ARTIST_KEY, artist);
+                    startActivity(intent);
+                } else {
+                    Log.e("ArtistOverviewActivity", "Artist is null before navigating to EditArtistActivity.");
+                }
+
+            }
+        });
     }
 }
