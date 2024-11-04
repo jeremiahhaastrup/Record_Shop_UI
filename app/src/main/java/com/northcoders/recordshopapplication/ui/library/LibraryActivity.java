cb7f4aa8603cffa75f4570c1890ecfdfd8976875
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-11-04 12:35:44 +0000

    feat: LibraryActivity implemented

--- app/src/main/java/com/northcoders/recordshopapplication/ui/library/LibraryActivity.java
+++ app/src/main/java/com/northcoders/recordshopapplication/ui/library/LibraryActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -24,7 +23,6 @@ public class LibraryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_library);
 
         activityLibraryBinding = DataBindingUtil.setContentView(this, R.layout.activity_library);
